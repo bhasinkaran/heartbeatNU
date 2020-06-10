a980b9698cca57aa2426f6f40d76fceba6c1d9ea
2022karanbhasin <44887140+2022karanbhasin@users.noreply.github.com>
2020-06-10 21:49:39 +0530

    refactored into components and removed hash from url for homepage

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -73,86 +73,23 @@
 import axios from 'axios'
 
 import React, {useState} from 'react';
+import {Router } from  'react-router-dom'
 
 import logo from './logo.svg';
 import './App.css';
 import Spotify from 'spotify-web-api-js';
+import Homepage from './components/homepage';
+import {BrowserRouter, Route} from 'react-router-dom'
 const s = new Spotify();
 
 function App() {
-  function getHashParams() {
-    var x=4
-    var hashParams = {};
-    var e, r = /([^&;=]+)=?([^&;]*)/g,
-        q = window.location.hash.substring(1);
-    while ( e = r.exec(q)) {
-       hashParams[e[1]] = decodeURIComponent(e[2]);
-    }
-    return hashParams;
-  }
-  var hashParams=getHashParams()
-  var temp=false;
-  if( hashParams.access_token )
-  {
-    temp=true;
-    s.setAccessToken(hashParams.access_token)
-  }
 
-
-  const [loggedIn, setLoggedIn]=useState(temp)
-  const[nowPlaying, setNowPlaying]=useState({name: "not checked",image:""})
-  const [artists,setArtists]=useState(['None'])
-  
-  function getNowPlaying(){
-    s.getMyCurrentPlaybackState()
-      .then((response)=>{
-        // console.log(response)
-        setNowPlaying({
-          name: response.item.name,
-          image: response.item.album.images[0].url
-        })
-      }
-      )
-  }
-  function getTopArtists(){
-    s.getMyTopArtists()
-    .then((response)=>{
-      console.log(response)
-      
-      setArtists(response.items)
-
-    })
-  }
- var redirectableLogin= process.env.NODE_ENV === 'production' ? `https://pure-harbor-26317.herokuapp.com/login` : `http://localhost:8888/login`;
-  
- return (
-    <div className="App">
-     
-    {!loggedIn ? 
-
-     <a href= {redirectableLogin }>
-
-      <button>Log in with spotify</button> 
-      </a>:
-      <div>
-      <div> Now Playing {nowPlaying.name}
-      </div>
-      <div>
-        <img src={nowPlaying.image} width="250" >
-        </img>
-      </div>
-      <div>{artists.map(obj=>obj['name'])}
-      </div>
-      <button onClick={()=>getNowPlaying()}>
-        Check Now Playing
-      </button>
-      <button onClick={()=>getTopArtists()}>
-        Check Top Artists
-      </button>
-      </div>
-    }
-    </div>
+  return(
+    <BrowserRouter>
+      <Route exact path="/:hash"  render = {()=> <Homepage ></Homepage>} />
+    </BrowserRouter>
   );
+ 
 }
 
 export default App;
---
+++ react-ui/src/components/homepage.js
@@ -0,0 +1,86 @@
+import axios from 'axios'
+
+import React, {useState} from 'react';
+import {Router , useParams} from  'react-router-dom'
+
+import Spotify from 'spotify-web-api-js';
+const s = new Spotify();
+
+const Homepage = () =>{
+  var {hash } = useParams();
+  function getHashParams() {
+    var x=4
+    var hashParams = {};
+    var e, r = /([^&;=]+)=?([^&;]*)/g,
+        q = hash.substring(1);
+    while ( e = r.exec(q)) {
+       hashParams[e[1]] = decodeURIComponent(e[2]);
+    }
+    return hashParams;
+  }
+  var hashParams=getHashParams()
+  var temp=false;
+  if( hash!="login" )
+  {
+    temp=true;
+    s.setAccessToken(hash)
+  }
+
+
+  const [loggedIn, setLoggedIn]=useState(temp)
+  const[nowPlaying, setNowPlaying]=useState({name: "not checked",image:""})
+  const [artists,setArtists]=useState(['None'])
+  
+  function getNowPlaying(){
+    s.getMyCurrentPlaybackState()
+      .then((response)=>{
+        // console.log(response)
+        setNowPlaying({
+          name: response.item.name,
+          image: response.item.album.images[0].url
+        })
+      }
+      )
+  }
+  function getTopArtists(){
+    s.getMyTopArtists()
+    .then((response)=>{
+      console.log(response)
+      
+      setArtists(response.items)
+
+    })
+  }
+ var redirectableLogin= process.env.NODE_ENV === 'production' ? `https://pure-harbor-26317.herokuapp.com/login` : `http://localhost:8888/login`;
+  
+ return (
+    <div className="App">
+     
+    {!loggedIn ? 
+
+     <a href= {redirectableLogin } onClick = {() => setLoggedIn(true)}>
+
+      <button>Log in with spotify</button> 
+      </a>:
+      <div>
+      <div> Now Playing {nowPlaying.name}
+      </div>
+      <div>
+        <img src={nowPlaying.image} width="250" >
+        </img>
+      </div>
+      <div>{artists.map(obj=>obj['name'])}
+      </div>
+      <button onClick={()=>getNowPlaying()}>
+        Check Now Playing
+      </button>
+      <button onClick={()=>getTopArtists()}>
+        Check Top Artists
+      </button>
+      </div>
+    }
+    </div>
+  );
+}
+
+export default Homepage;
