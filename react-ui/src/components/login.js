79154a4f8396fd35dd389e176f7bf1fca2c62d18
2022karanbhasin <44887140+2022karanbhasin@users.noreply.github.com>
2020-06-13 04:23:41 +0530

    Revert "Login Page First Draft"
    This reverts commit 043b64b04751e05d46cba11484e0cd3e5be14971.

--- react-ui/src/components/login.js
+++ react-ui/src/components/login.js
@@ -1,84 +1,24 @@
-import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Paper from '@material-ui/core/Paper';
-import Box from '@material-ui/core/Box';
-import Grid from '@material-ui/core/Grid';
-import LockIcon from '@material-ui/icons/Lock';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import green from '@material-ui/core/colors/green';
-const primary = green[900]; // #f44336
+
+import React, {useState} from 'react';
 
 
 
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    height: '100vh',
-  },
-  image: {
-    backgroundImage: 'url(https://i.ibb.co/pQBpgMz/streaks-black-stripes-green-lines-3840x2160-c2-000000-32cd32-l2-113-113-a-135-f-1.jpg)',
-    backgroundRepeat: 'no-repeat',
-    backgroundColor:
-      theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-  },
-  paper: {
-    margin: theme.spacing(8, 4),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-}));
-
-export default function SignInSide() {
-  const classes = useStyles();
-  var redirectableLogin= process.env.NODE_ENV === 'production' ? `https://pure-harbor-26317.herokuapp.com/login` : `http://localhost:8888/login`;
-
-  return (
-    <Grid container component="main" className={classes.root}>
-      <CssBaseline />
+const Login = () =>{
+  
+ var redirectableLogin= process.env.NODE_ENV === 'production' ? `https://pure-harbor-26317.herokuapp.com/login` : `http://localhost:8888/login`;
+  
+ return (
+  
+    <div >
       
-      <Grid item xs={false} sm={4} md={7} className={classes.image} />
-      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-        <div className={classes.paper} style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '85vh'}}>
-          
-          <Avatar className={classes.dark}>
-            <LockIcon style={{ color: 'green'}} />
-          </Avatar>
-         
 
-            <a href= {redirectableLogin }>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-            >
-              Log in with spotify
-            </Button>
-             </a>
-        </div>
-      </Grid>
-    </Grid>
+     <a href= {redirectableLogin }>
+    
+      <button>Log in with spotify</button> 
+      </a> 
+      </div>
+      
   );
 }
+
+export default Login;
