0a901c0c104f963c1fae73207c5018c15a649005
2022karanbhasin <44887140+2022karanbhasin@users.noreply.github.com>
2020-06-23 13:48:36 +0530

    Commented out the location-dependent useEffect

--- react-ui/src/components/homepage.js
+++ react-ui/src/components/homepage.js
@@ -76,45 +76,45 @@ useEffect(attractedTo, [mongouser, allusers]);
         setAttracted(allusers.filter(item => item.gender == mongouser.type && item.id!=mongouser.id));
       }
     };
-useEffect(rankAttractedTo, [attractedTo]);
-function rankAttractedTo(){
-  function comparedistance(a,b){
-    let lat1=mongouser.location[0];
-    let lon1=mongouser.location[1];
-    let distance1=distance(lat1,lon1,a);
-    let distance2=distance(lat1,lon1,b);
-    return distance2-distance1;
-  }
-  function distance(lat1, lon1,user2) {
-    let lat2 = user2.location[0];
-    let lon2=user2.location[1];
-    if ((lat1 == lat2) && (lon1 == lon2)) {
-      return 0;
-    }
-    else {
-      var radlat1 = Math.PI * lat1/180;
-      var radlat2 = Math.PI * lat2/180;
-      var theta = lon1-lon2;
-      var radtheta = Math.PI * theta/180;
-      var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
-      if (dist > 1) {
-        dist = 1;
-      }
-      dist = Math.acos(dist);
-      dist = dist * 180/Math.PI;
-      dist = dist * 60 * 1.1515;
-      return dist;
-    }
+// useEffect(rankAttractedTo, [attractedTo]);
+// function rankAttractedTo(){
+//   function comparedistance(a,b){
+//     let lat1=mongouser.location[0];
+//     let lon1=mongouser.location[1];
+//     let distance1=distance(lat1,lon1,a);
+//     let distance2=distance(lat1,lon1,b);
+//     return distance2-distance1;
+//   }
+//   function distance(lat1, lon1,user2) {
+//     let lat2 = user2.location[0];
+//     let lon2=user2.location[1];
+//     if ((lat1 == lat2) && (lon1 == lon2)) {
+//       return 0;
+//     }
+//     else {
+//       var radlat1 = Math.PI * lat1/180;
+//       var radlat2 = Math.PI * lat2/180;
+//       var theta = lon1-lon2;
+//       var radtheta = Math.PI * theta/180;
+//       var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+//       if (dist > 1) {
+//         dist = 1;
+//       }
+//       dist = Math.acos(dist);
+//       dist = dist * 180/Math.PI;
+//       dist = dist * 60 * 1.1515;
+//       return dist;
+//     }
    
-  }
-  // console.log(attractedUsers);
-  var copyUsers = attractedUsers;
-  if(attractedUsers){
-    copyUsers.sort(comparedistance);
-    setAttracted(copyUsers);
-  }
+//   }
+//   // console.log(attractedUsers);
+//   var copyUsers = attractedUsers;
+//   if(attractedUsers){
+//     copyUsers.sort(comparedistance);
+//     setAttracted(copyUsers);
+//   }
   
-}
+// }
 
 async function  handleSearchChange(valuee)  {
   setisLoading(true);
