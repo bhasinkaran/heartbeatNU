111854ddb1c729af9c48784ba37e9fec41d50f2e
Vir Narula <virsvirus@gmail.com>
2020-08-21 00:12:58 -0500

    Created comment set up for profile page

--- react-ui/src/dating_components/ProfileHomepage.js
+++ react-ui/src/dating_components/ProfileHomepage.js
@@ -1,8 +1,34 @@
 import React from 'react'
+import {Grid} from 'semantic-ui-react'
+import "./ProfileHomepage.css"
 
 class ProfileHomepage extends React.Component {
-    render () {
-        return(<h1> This is the profile page </h1>);
+    render() {
+        return (
+            <Grid columns={2}>
+                <Grid.Row>
+                    <Grid.Column>
+                        <h1>Editable Fields</h1>
+                        {/* Something similar to the sign up here */}
+                        {/* Options to add favorite songs/albums here */}
+                    </Grid.Column>
+                    <Grid.Column>
+                        <h1>This is how other people see you</h1>
+                        <Grid.Row columns={2}>
+                            <Grid.Column>
+                                {/* Name and other details here */}
+                            </Grid.Column>
+                            <Grid.Column>
+                                {/* Profile picture here */}
+                            </Grid.Column>
+                        </Grid.Row>
+                        <Grid.Row>
+                            {/* Grid of cards with questions here */}
+                        </Grid.Row>
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
+        );
     }
 }
 
